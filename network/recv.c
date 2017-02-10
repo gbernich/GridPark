5850c16c79f1d63ba042b7c73d38317e402957f3
Garrett Bernichon <garrettbernichon@Garretts-MacBook-Pro.local>
2017-02-10 14:28:08 -0500

    Commented files.

--- network/recv.c
+++ network/recv.c
@@ -1,50 +1,66 @@
-#include <sys/socket.h>
-#include <sys/types.h>
-#include <netinet/in.h>
-#include <netdb.h>
+//-----------------------------------------------------------------------------
+// File:                recv.c
+// Author:              Garrett Bernichon
+// Function:            To receive data and write it to the necessary
+//                      mySQL database table.
+// Command Line Args:   
+//-----------------------------------------------------------------------------
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <errno.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <netdb.h>
 #include <arpa/inet.h> 
+
+#include "network.c"
 
 int main(int argc, char *argv[])
 {
-    int sockfd = 0, n = 0;
-    char recvBuff[1024];
+    int sockfd = 0;
+    int n = 0;
+    char recvBuff[1024] = {0];
     struct sockaddr_in serv_addr; 
 
+    // Check for command line inputs
     if(argc != 2)
     {
         printf("\n Usage: %s <ip of server> \n",argv[0]);
         return 1;
     } 
 
-    memset(recvBuff, '0',sizeof(recvBuff));
+    // Create the socket
     if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
     {
         printf("\n Error : Could not create socket \n");
         return 1;
     } 
 
+    // Zero out serv_addr struct
     memset(&serv_addr, '0', sizeof(serv_addr)); 
 
+    // Set up socket
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_port = htons(5000); 
 
+    // Convert IP address to binary
     if(inet_pton(AF_INET, argv[1], &serv_addr.sin_addr)<=0)
     {
         printf("\n inet_pton error occured\n");
         return 1;
     } 
 
+    // Connect the socket to the network
     if( connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
     {
        printf("\n Error : Connect Failed \n");
        return 1;
     } 
 
+    // Receive the packets until done, write to command line
     while ( (n = read(sockfd, recvBuff, sizeof(recvBuff)-1)) > 0)
     {
         recvBuff[n] = 0;
@@ -54,6 +70,7 @@ int main(int argc, char *argv[])
         }
     } 
 
+    // Error reading from socket
     if(n < 0)
     {
         printf("\n Read error \n");
--- network/send.c
+++ network/send.c
@@ -1,43 +1,60 @@
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
+//-----------------------------------------------------------------------------
+// File:                send.c
+// Author:              Garrett Bernichon
+// Function:            To send data when it is available so the recv process
+//                      can update its mySQL database table.
+// Command Line Args:   
+//-----------------------------------------------------------------------------
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <errno.h>
-#include <string.h>
+#include <sys/socket.h>
 #include <sys/types.h>
-#include <time.h> 
+#include <netinet/in.h>
+#include <netdb.h>
+#include <arpa/inet.h> 
+
+#include "network.c"
 
 int main(int argc, char *argv[])
 {
-    int listenfd = 0, connfd = 0;
-    struct sockaddr_in serv_addr; 
-
-    char sendBuff[1025];
+    int listenfd = 0;
+    int connfd = 0;
+    struct sockaddr_in serv_addr;
+    char sendBuff[1025] = {0};
     time_t ticks; 
 
+    // Open socket to listen on
     listenfd = socket(AF_INET, SOCK_STREAM, 0);
-    memset(&serv_addr, '0', sizeof(serv_addr));
-    memset(sendBuff, '0', sizeof(sendBuff)); 
+    memset(&serv_addr, 0 , sizeof(serv_addr));
 
+    // Set up parameters for the connection
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
     serv_addr.sin_port = htons(5000); 
-
     bind(listenfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)); 
 
+    // Listen for up to 10 connections
     listen(listenfd, 10); 
 
     while(1)
     {
+        // Accept request to connect
         connfd = accept(listenfd, (struct sockaddr*)NULL, NULL); 
 
+        // Create a packet to send
         ticks = time(NULL);
         snprintf(sendBuff, sizeof(sendBuff), "%.24s\r\n", ctime(&ticks));
+
+        // Send packet over socket
         write(connfd, sendBuff, strlen(sendBuff)); 
 
+        // Close connection
         close(connfd);
+
+        // Sleep for some time
         sleep(1);
      }
 }
