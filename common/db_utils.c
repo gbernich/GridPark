9eab16a1ad7483279935d2af4b7a8de9580b4060
Garrett Bernichon <bernichg@gmail.com>
2017-02-10 19:32:06 -0500

    Initial commit

---
+++ common/db_utils.c
@@ -0,0 +1,10 @@
+//-----------------------------------------------------------------------------
+// File:                db_utils.c
+// Author:              Garrett Bernichon
+// Function:            Provide functions to interact with the mySQL databases.
+//-----------------------------------------------------------------------------
+
+int CreateNewSpot(int spot_id, int region, int distance, int * corners)
+{
+    return 0;
+}
---
+++ common/db_utils.h
@@ -0,0 +1,7 @@
+//-----------------------------------------------------------------------------
+// File:                db_util.h
+// Author:              Garrett Bernichon
+// Function:            Provide functions to interact with the mySQL databases.
+//-----------------------------------------------------------------------------
+
+int CreateNewSpot(int spot_id, int region, int distance, int * corners);
