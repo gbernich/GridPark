12296675571637fb633eaa391164f74f3f1f299c
Garrett Bernichon <garrettbernichon@Garretts-MacBook-Pro.local>
2017-04-24 20:52:15 -0400

    Fixing Cooksie image to map translation

--- ip.cpp
+++ ip.cpp
@@ -1153,7 +1153,7 @@ int GetYPositionOfSpot(int regionId, int start)
   case (K_COOKSIE_NW_ID):
     lengthOfSub = 135;
     regionEnd = 490;
-    pseudoStart = (K_COOKSIE_NW_WIN_START_TP_Y - start) * (K_COOKSIE_NW_WIN_END_TP_Y - K_COOKSIE_NW_WIN_START_TP_Y) / (float)(K_COOKSIE_NW_WIN_END_TP_X - K_COOKSIE_NW_WIN_START_TP_X) + K_COOKSIE_NW_WIN_START_TP_Y;
+    pseudoStart = (start - K_COOKSIE_NW_WIN_START_TP_Y) * (K_COOKSIE_NW_WIN_END_TP_Y - K_COOKSIE_NW_WIN_START_TP_Y) / (float)(K_COOKSIE_NW_WIN_END_TP_X - K_COOKSIE_NW_WIN_START_TP_X) + K_COOKSIE_NW_WIN_START_TP_Y;
     withinSub = (pseudoStart - K_COOKSIE_NW_WIN_START_TP_Y) / (float)(K_COOKSIE_NW_WIN_END_TP_Y - K_COOKSIE_NW_WIN_START_TP_Y);
     cout << "start " << start << endl;
     cout << "slope " << (K_COOKSIE_NW_WIN_END_TP_Y - K_COOKSIE_NW_WIN_START_TP_Y)/(float)(K_COOKSIE_NW_WIN_END_TP_X - K_COOKSIE_NW_WIN_START_TP_X) << endl;
